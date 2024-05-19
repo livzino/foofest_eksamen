e3999236d68be6a5e96d38d1b42b4c2c9ab2c833
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-19 15:17:18 +0200

    Stylet knappen overfladisk

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+import Button from "../components/Button";
+export default async function Home() {
+  return (
+    <main>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Button></Button>
+    </main>
+  );
+}
