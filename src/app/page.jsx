6f3fdeea4e538a73dd03482c96610adfb13ac3d8
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-21 11:55:00 +0200

    stylet title

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,14 +2,10 @@
 import "./globals.css";
 import Button from "../components/Button";
 import CountDown from "../components/CountDown";
+import Title from "../components/Title";
 
 export default function Home() {
   const targetDate = new Date("Jun 26, 2024").getTime();
 
-  return (
-    <main>
-      <CountDown targetDate={targetDate} />
-      <Button />
-    </main>
-  );
+  return <main></main>;
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <div className="titlecontainer">
+      <div class="titletxt">Tickets</div>
+      <div className="titleline"></div>
+    </div>
+  );
+}
