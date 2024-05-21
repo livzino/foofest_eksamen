2888cc5e59f3a99cdcbd3a09ac9ee2ceced28415
Liv <livbendix@live.dk>
2024-05-21 12:10:46 +0200

    jheks

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
 "use client";
 import "./globals.css";
+import CountDown from "../components/CountDown";
+import Title from "../components/Title";
 
-import Button from "../components/Button";
-<<<<<<< HEAD
 import Ticket from "../components/Ticket";
 import Vip from "../components/Vip";
 export default async function Home() {
+  const targetDate = new Date("Jun 26, 2024").getTime();
   return (
     <main>
       <div></div>
@@ -18,13 +19,4 @@ export default async function Home() {
       <Button></Button>
     </main>
   );
-=======
-import CountDown from "../components/CountDown";
-import Title from "../components/Title";
-
-export default function Home() {
-  const targetDate = new Date("Jun 26, 2024").getTime();
-
-  return <main></main>;
->>>>>>> 6f3fdeea4e538a73dd03482c96610adfb13ac3d8
 }
