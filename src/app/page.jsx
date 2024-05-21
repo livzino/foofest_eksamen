a7edceb7e862f4b9decafed8675a96fc5cc55f9e
Liv <livbendix@live.dk>
2024-05-21 12:02:31 +0200

    tickets

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,14 @@
 import "./globals.css";
+
 import Button from "../components/Button";
+import Ticket from "../components/Ticket";
+import Vip from "../components/Vip";
 export default async function Home() {
   return (
     <main>
+      <div></div>
+      <Vip></Vip>
+      <Ticket></Ticket>
       <br />
       <br />
       <br />
