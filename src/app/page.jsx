77554b2720fef10771c4f65417e7a077ff0aff0d
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-21 12:09:29 +0200

    tjek

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,6 @@
 "use client";
 import "./globals.css";
-
 import Button from "../components/Button";
-<<<<<<< HEAD
-import Ticket from "../components/Ticket";
-import Vip from "../components/Vip";
-export default async function Home() {
-  return (
-    <main>
-      <div></div>
-      <Vip></Vip>
-      <Ticket></Ticket>
-      <br />
-      <br />
-      <br />
-      <br />
-      <Button></Button>
-    </main>
-  );
-=======
 import CountDown from "../components/CountDown";
 import Title from "../components/Title";
 
@@ -26,5 +8,4 @@ export default function Home() {
   const targetDate = new Date("Jun 26, 2024").getTime();
 
   return <main></main>;
->>>>>>> 6f3fdeea4e538a73dd03482c96610adfb13ac3d8
 }
