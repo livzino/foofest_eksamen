944686b079b6e1ea7dc0073a6ef67b848da79a9e
kezia-hansen <131632957+kezia-hansen@users.noreply.github.com>
2024-05-21 12:30:13 +0200

    igang med footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,21 +2,14 @@
 import "./globals.css";
 import CountDown from "../components/CountDown";
 import Title from "../components/Title";
-
 import Ticket from "../components/Ticket";
 import Vip from "../components/Vip";
-export default async function Home() {
+import Footer from "../components/Footer";
+export default function Home() {
   const targetDate = new Date("Jun 26, 2024").getTime();
   return (
     <main>
-      <div></div>
-      <Vip></Vip>
-      <Ticket></Ticket>
-      <br />
-      <br />
-      <br />
-      <br />
-      <Button></Button>
+      <Footer />
     </main>
   );
 }
